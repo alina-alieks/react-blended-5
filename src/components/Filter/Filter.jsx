c9cc746bed8ae40de8449a88c48263621279e3eb
fulaytar <fulaytar2405@gmail.com>
2024-06-02 12:32:03 +0300

    done!

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,9 +1,16 @@
+import { useDispatch } from 'react-redux';
 import styles from './Filter.module.css';
+import { setFilter } from 'reduxState/currencySlice';
 export const Filter = () => {
+  const dispatch = useDispatch();
+  const onChangeName = e => {
+    dispatch(setFilter(e.target.value.toLowerCase()));
+  };
   return (
     <input
       placeholder="What currency are you looking for?🧐"
       className={styles.input}
+      onChange={onChangeName}
     />
   );
 };
--- src/reduxState/selector.js
+++ src/reduxState/selector.js
@@ -5,11 +5,14 @@ export const selectExchangeInfo = state => state.currency.exchangeInfo;
 export const selectError = state => state.currency.isError;
 export const selectLoading = state => state.currency.isLoading;
 export const selectRates = state => state.currency.rates;
+export const selectFilter = state => state.currency.filter;
 export const selectFiltredRates = createSelector(
-  [selectorBaseCurrency, selectRates],
-  (baseCurrency, rates) => {
+  [selectorBaseCurrency, selectRates, selectFilter],
+  (baseCurrency, rates, filter) => {
     return rates
-      .filter(([key]) => key !== baseCurrency)
+      .filter(
+        ([key]) => key !== baseCurrency && key.toLowerCase().includes(filter),
+      )
       .map(([key, value]) => ({ key, value: (1 / value).toFixed(2) }));
   },
 );
